7842eb81143fbfe2ccea28e1cd8fc94ef02e92fa
marcoruch <marco_ruch@gmx.ch>
2019-12-29 01:08:54 +0100

    Deleted some Stuff

--- 1_HelloWorld/target/debug/deps/hello_world.d
+++
@@ -1,5 +0,0 @@
-E:\Projekte\Rusty\Rust-Trial\Apps\1_HelloWorld\target\debug\deps\hello_world.exe: src\main.rs
-
-E:\Projekte\Rusty\Rust-Trial\Apps\1_HelloWorld\target\debug\deps\hello_world.d: src\main.rs
-
-src\main.rs:
--- 1_HelloWorld/target/debug/hello-world.d
+++
@@ -1,1 +0,0 @@
-E:\Projekte\Rusty\Rust-Trial\1_HelloWorld\target\debug\hello-world.exe: E:\Projekte\Rusty\Rust-Trial\1_HelloWorld\src\main.rs
