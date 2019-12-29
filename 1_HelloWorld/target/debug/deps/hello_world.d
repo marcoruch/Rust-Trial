19b46438ca4e756c289bf56e3e300d8222b2fe53
marcoruch <marco_ruch@gmx.ch>
2019-12-29 01:06:06 +0100

    Initial Commit

---
+++ 1_HelloWorld/target/debug/deps/hello_world.d
@@ -0,0 +1,5 @@
+E:\Projekte\Rusty\Rust-Trial\Apps\1_HelloWorld\target\debug\deps\hello_world.exe: src\main.rs
+
+E:\Projekte\Rusty\Rust-Trial\Apps\1_HelloWorld\target\debug\deps\hello_world.d: src\main.rs
+
+src\main.rs:
---
+++ 1_HelloWorld/target/debug/hello-world.d
@@ -0,0 +1,1 @@
+E:\Projekte\Rusty\Rust-Trial\1_HelloWorld\target\debug\hello-world.exe: E:\Projekte\Rusty\Rust-Trial\1_HelloWorld\src\main.rs
